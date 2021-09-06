9572b8c66bc9f78806207829a62d3a2022fbea40
knmarcin <Imspy4pcp@>
2021-09-06 13:12:27 +0200

    Add model for car

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    make = models.CharField(max_length=128)
+    model = models.CharField(max_length=128)
+    avg_rating = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.id}"
