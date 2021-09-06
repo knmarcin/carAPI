0ef9f3e829849b1d4706436cabe7439824c9f57c
knmarcin <Imspy4pcp@>
2021-09-06 13:30:03 +0200

    Add car get endpoint

--- CarAPI/urls.py
+++ CarAPI/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cars.views import CarsViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cars/', CarsViewSet.as_view()),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from cars.models import Car
+from cars.serializers import CarSerializer
 
-# Create your views here.
+
+class CarsViewSet(APIView):
+    def get(self, *args, **kwargs):
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        if serializer.data:
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
