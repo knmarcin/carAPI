303ea9e0f85c0586f99f31cfd0e90adbc1930d0a
knmarcin <Imspy4pcp@>
2021-09-06 18:37:07 +0200

    Validator change

--- utils/rating_validator.py
+++ utils/rating_validator.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 
-def rating_validator(val: int ):
-    if not isinstance(val, int):
-        raise serializers.ValidationError({'rating':'Value must be an integer'})
-    elif (val > 5) or (val < 1):
-        raise serializers.ValidationError({'rating':'Value must be between 1 and 5'})
+def rating_validator(val: int) -> int:
+    if isinstance(val, int):
+        if (val > 5) or (val < 1):
+            raise serializers.ValidationError({'rating':'Value must be between 1 and 5'})
+        else:
+            return int(val)
     else:
-        return int(val)
+        raise serializers.ValidationError({'rating':'Value must be an integer'})
