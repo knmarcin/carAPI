d1475d15df87d55b94b96b488a9a31c0246ad48b
knmarcin <Imspy4pcp@>
2021-09-07 11:23:03 +0200

    Prepare for deploy

--- CarAPI/settings.py
+++ CarAPI/settings.py
@@ -16,7 +16,6 @@ import dj_database_url
 import django_heroku
 import os
 
-django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
